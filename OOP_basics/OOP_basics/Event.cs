49993c042defd6c09cfc80e2077919713ee35d67
tbalic <balic.tihana@gmail.com>
2020-12-04 15:11:59 +0100

    Added new class Event

---
+++ OOP_basics/OOP_basics/Event.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_basics
+{
+    public class Event
+    {
+        public Event(string name, string eventType, string startTime, string endTime )
+        {
+            Name = name;
+            EventType = eventType;
+            StartTime = startTime;
+            EndTime = endTime;
+
+        }
+
+        public string Name { get; set; }
+        public string EventType { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
+    }
+}
