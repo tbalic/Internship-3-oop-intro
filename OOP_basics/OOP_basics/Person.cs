cf5e0abdc2e827223978bc85f33f09ac9c21e676
tbalic <balic.tihana@gmail.com>
2020-12-04 15:01:48 +0100

    Added new class Person

---
+++ OOP_basics/OOP_basics/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_basics
+{
+    public class Person
+    {
+        public Person(string firstName, string lastName, int oib, int phoneNumber)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            OIB = oib;
+            PhoneNumber = phoneNumber;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int OIB { get; set; }
+        public int PhoneNumber { get; set; }
+    }
+}
